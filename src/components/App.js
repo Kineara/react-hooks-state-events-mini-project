e08bddbe2a8a977e0dfa945a8f7cf760d48dd31e
Lauren Hoock <laurenh8916@protonmail.com>
2022-06-11 23:05:21 -0600

    added filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,61 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log( CATEGORIES );
+//console.log("Here's the data you're working with");
+//console.log(CATEGORIES);
+
 
 function App() {
-  const {tasks, setTasks} = useState({TASKS})
-  const {newItemText, setNewItemText} = useState("")
-  const {newItemCategory, setNewItemCategory} = useState("All")
+  const [tasks, setTasks] = useState(TASKS);
+  const [newItemText, setNewItemText] = useState("");
+  const [newItemCategory, setNewItemCategory] = useState("All");
+  const [filterState, setFilterState] = useState("All");
 
   function submitHandler() {
-    const newTaskList = [...TASKS, {text: newItemText, category: newItemCategory}]
-    setTasks(newTaskList) 
+    const newTaskList = [
+      ...TASKS,
+      { text: newItemText, category: newItemCategory },
+    ];
+    setTasks(newTaskList);
   }
-  
+
+  function filterTasks() {
+    if (filterState==="All") {
+      return tasks;
+    } else {
+      return tasks.filter(task => task.category===filterState)
+    }
+  }
+
+  const filteredTasks = filterTasks();
+
+  console.log(filteredTasks);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter categories={CATEGORIES}/>
-      <NewTaskForm categories={CATEGORIES} newItemText={newItemText} newItemCategory={newItemCategory} onTextChange={setNewItemText} onCategoryChange={setNewItemCategory} onSubmit={submitHandler}
-        />
-      <TaskList tasks={TASKS} />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedFilter={filterState}
+        clickHandler={(e) => {
+          const buttons = Array.from(e.target.parentElement.children);
+          buttons.forEach((btn) => btn.classList.remove("selected"));
+          e.target.classList.add("selected");
+          setFilterState(e.target.id);
+        }}
+      />
+      <NewTaskForm
+        categories={CATEGORIES}
+        newItemText={newItemText}
+        newItemCategory={newItemCategory}
+        onTextChange={setNewItemText}
+        onCategoryChange={setNewItemCategory}
+        onSubmit={submitHandler}
+      />
+      <TaskList tasks={filteredTasks} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-function CategoryFilter({categories}) {
+function CategoryFilter({ categories, clickHandler, selectedFilter }) {
   const categoriesEls = categories.map((category) => {
-    return <button id={category}>{category}</button>
-  })
-                                       
+    return (
+      <button
+        id={category}
+        className={category === selectedFilter ? "selected" : null}
+        onClick={clickHandler}
+      >
+        {category}
+      </button>
+    );
+  });
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
